3383cc2e6c6858ab4dcd2ef9a8fdc4a297ebab7a
waiting <1661926154@qq.com>
2017-09-06 18:18:35 +0800

    Change data structure of ajax response

--- src/app/controller/idc.ts
+++ src/app/controller/idc.ts
@@ -1,27 +1,27 @@
-import {Controller} from 'egg';
+import {AjaxResp, Controller} from 'egg';
 import * as path from 'path';
 
 
 export default class IdcController extends Controller {
     public async read() {
         const {ctx, app} = this;
-        const resp: any = {
+        const resp: AjaxResp = {
             err: 0,
             dat: {
-                idData: '',
-                imgUrl: '',
+                info: '',
+                composite: '',
             },
         };
         const data = await ctx.service.idc.getData(app.config.idc);
 
         if (data) {
-            resp.dat.idData = data;
+            resp.dat.info = data;
 
             if (app.config.idc.dllImage && data.imagePath) {
                 const key = path.basename(data.imagePath);
 
                 app.idinfoMap.set(key, data);
-                resp.dat.imgUrl = await ctx.service.img.composite(data); // dataURL
+                resp.dat.composite = await ctx.service.img.composite(data); // dataURL
                 app.idinfoMap.delete(key);
             }
         }
